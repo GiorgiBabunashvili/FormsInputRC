1b119b1ae35dd7302a3dad4ccc9aa9dfb5b90d73
shanME <giorgie103188@gmail.com>
2024-08-07 18:09:10 +0400

    signup component get value

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header.jsx";
 import Login from "./components/Login.jsx";
 import LoginREF from "./components/LoginREF.jsx";
+import Signup from "./components/Signup.jsx";
 
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
       <Header />
       <main>
         {/* <Login /> */}
-        <LoginREF />
+        {/* <LoginREF /> */}
+        <Signup />
       </main>
     </>
   );
