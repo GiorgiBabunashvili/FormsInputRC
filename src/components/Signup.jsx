06e7b022892ab919bcfa571c46723d86ee7e8e96
shanME <giorgie103188@gmail.com>
2024-08-07 18:20:13 +0400

    FormData

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,16 @@
 export default function Signup() {
+  function hanldeSubmit(e) {
+    e.preventDefault();
+
+    const fd = new FormData(e.target);
+    const acquisitionChanel = fd.getAll("acquisition");
+    const data = Object.fromEntries(fd.entries());
+    data.acquisition = acquisitionChanel;
+    console.log(data);
+  }
+
   return (
-    <form>
+    <form onSubmit={hanldeSubmit}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
