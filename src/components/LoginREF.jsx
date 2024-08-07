d238babec7272a9ac52a872d0a5db9be2aa981a1
shanME <giorgie103188@gmail.com>
2024-08-07 18:02:18 +0400

    useRef

---
+++ src/components/LoginREF.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+export default function Login() {
+  const [enteredValue, setEnteredValue] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(enteredValue);
+  }
+
+  function handleChange(identifier, newValue) {
+    setEnteredValue((prevValue) => {
+      return {
+        ...prevValue,
+        [identifier]: newValue,
+      };
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={enteredValue.email}
+            onChange={(e) => handleChange("email", e.target.value)}
+          />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={enteredValue.password}
+            onChange={(e) => handleChange("password", e.target.value)}
+          />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
